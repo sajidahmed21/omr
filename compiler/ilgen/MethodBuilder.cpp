86658177cb1c2ec3b390284059fede664fbc506b
Sajid Ahmed <Syed.Sajid.Ahmed23@ibm.com>
2017-06-27 14:03:33 -0400

    Change _symbolNameFromSlot hash table to std::map
    Changed _symbolNameFromSlot to be a std::map instead of a (custom)
    hash table. This guarantees better type safety and helps in
    simplifying the JitBuilder interface.
    
    Signed-off-by: Sajid Ahmed <Syed.Sajid.Ahmed23@ibm.com>

--- compiler/ilgen/MethodBuilder.cpp
+++ compiler/ilgen/MethodBuilder.cpp
@@ -37,7 +37,6 @@
 #include "control/Recompilation.hpp"
 #include "infra/Assert.hpp"
 #include "infra/Cfg.hpp"
-#include "infra/HashTab.hpp"
 #include "infra/STLUtils.hpp"
 #include "infra/List.hpp"
 #include "ilgen/IlGeneratorMethodDetails_inlines.hpp"
@@ -97,7 +96,7 @@ MethodBuilder::MethodBuilder(TR::TypeDictionary *types, OMR::VirtualMachineState
    _symbols(str_comparator, *_memoryRegion),
    _parameterSlot(str_comparator, *_memoryRegion),
    _symbolTypes(str_comparator, *_memoryRegion),
-   _symbolNameFromSlot(new (*_memoryRegion) TR_HashTabInt(_trMemory)),
+   _symbolNameFromSlot(std::less<int32_t>(), *_memoryRegion),
    _symbolIsArray(str_comparator, *_memoryRegion),
    _memoryLocations(str_comparator, *_memoryRegion),
    _functions(str_comparator, *_memoryRegion),
@@ -152,6 +151,7 @@ MethodBuilder::~MethodBuilder()
    _symbols.clear();
    _parameterSlot.clear();
    _symbolTypes.clear();
+   _symbolNameFromSlot.clear();
    _symbolIsArray.clear();
    _memoryLocations.clear();
    _functions.clear();
@@ -357,9 +357,7 @@ void
 MethodBuilder::defineSymbol(const char *name, TR::SymbolReference *symRef)
    {
    _symbols.insert(std::make_pair(name, symRef));
-
-   TR_HashId id=0;
-   _symbolNameFromSlot->add(symRef->getCPIndex(), id, (void *)name);
+   _symbolNameFromSlot.insert(std::make_pair(symRef->getCPIndex(), name));
    
    TR::IlType *type = typeDictionary()->PrimitiveType(symRef->getSymbol()->getDataType());
    _symbolTypes.insert(std::make_pair(name, type));
@@ -400,8 +398,7 @@ MethodBuilder::lookupSymbol(const char *name)
       {
       symRef = symRefTab()->createTemporary(_methodSymbol, primitiveType);
       symRef->getSymbol()->getAutoSymbol()->setName(name);
-      TR_HashId nameFromSlotID;
-      _symbolNameFromSlot->add(symRef->getCPIndex(), nameFromSlotID, (void *)name);
+      _symbolNameFromSlot.insert(std::make_pair(symRef->getCPIndex(), name));
       }
    symRef->getSymbol()->setNotCollected();
 
@@ -471,8 +468,8 @@ void
 MethodBuilder::DefineMemory(const char *name, TR::IlType *dt, void *location)
    {
    MB_REPLAY("DefineMemory(\"%s\", %s, " REPLAY_POINTER_FMT ");", name, REPLAY_TYPE(dt), REPLAY_POINTER(location, name));
-   _symbolTypes.insert(std::make_pair(name, dt));
 
+   _symbolTypes.insert(std::make_pair(name, dt));
    _memoryLocations.insert(std::make_pair(name, location));
    }
 
@@ -480,11 +477,9 @@ void
 MethodBuilder::DefineParameter(const char *name, TR::IlType *dt)
    {
    MB_REPLAY("DefineParameter(\"%s\", %s);", name, REPLAY_TYPE(dt));
+
    _parameterSlot.insert(std::make_pair(name, _numParameters));
-
-   TR_HashId nameFromSlotID;
-   _symbolNameFromSlot->add(_numParameters, nameFromSlotID, (void *) name);
-
+   _symbolNameFromSlot.insert(std::make_pair(_numParameters, name));
    _symbolTypes.insert(std::make_pair(name, dt));
 
    _numParameters++;
@@ -564,9 +559,9 @@ MethodBuilder::DefineFunction(const char* const name,
 const char *
 MethodBuilder::getSymbolName(int32_t slot)
    {
-   TR_HashId nameFromSlotID;
-   if (_symbolNameFromSlot->locate(slot, nameFromSlotID))
-      return (const char *)_symbolNameFromSlot->getData(nameFromSlotID);
+   SlotToSymNameMap::iterator it = _symbolNameFromSlot.find(slot);
+   if (it != _symbolNameFromSlot.end())
+      return it->second;
    return NULL;
    }
 
@@ -580,15 +575,13 @@ MethodBuilder::getParameterTypes()
    TR::IlType **paramTypesArray = _cachedParameterTypesArray;
    for (int32_t p=0;p < _numParameters;p++)
       {
-      TR_HashId nameFromSlotID;
-      _symbolNameFromSlot->locate(p, nameFromSlotID);
-      const char *name = (const char *) _symbolNameFromSlot->getData(nameFromSlotID);
+      SlotToSymNameMap::iterator symNamesIterator = _symbolNameFromSlot.find(p);
+      TR_ASSERT_FATAL(symNamesIterator != _symbolNameFromSlot.end(), "No symbol found in slot %d", p);
+      const char *name = symNamesIterator->second;
 
-      std::map<const char *, TR::IlType *, StrComparator>::iterator it = _symbolTypes.find(name);
-
-      TR_ASSERT_FATAL(it != _symbolTypes.end(), "No matching symbol type for parameter '%s'", name);
-
-      paramTypesArray[p] = it->second;
+      std::map<const char *, TR::IlType *, StrComparator>::iterator symTypesIterator = _symbolTypes.find(name);
+      TR_ASSERT_FATAL(symTypesIterator != _symbolTypes.end(), "No matching symbol type for parameter '%s'", name);
+      paramTypesArray[p] = symTypesIterator->second;
       }
 
    _cachedParameterTypes = paramTypesArray;
