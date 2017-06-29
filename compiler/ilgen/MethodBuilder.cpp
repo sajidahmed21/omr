118bd68ad19c99b5d2816f8f8e9c3bc10a545620
Sajid Ahmed <Syed.Sajid.Ahmed23@ibm.com>
2017-06-29 16:40:23 -0400

    Added fatal asserts to prevent duplicate names
    Added checks to stop duplicate symbols, functions, parameters, and
    memory locations from being defined. This will highlight errors and make
    debugging easier.
    
    Signed-off-by: Sajid Ahmed <Syed.Sajid.Ahmed23@ibm.com>

--- compiler/ilgen/MethodBuilder.cpp
+++ compiler/ilgen/MethodBuilder.cpp
@@ -334,6 +334,8 @@ MethodBuilder::symbolDefined(const char *name)
 void
 MethodBuilder::defineSymbol(const char *name, TR::SymbolReference *symRef)
    {
+   TR_ASSERT_FATAL(_symbols.find(name) == _symbols.end(), "Symbol '%s' already defined", name);
+
    _symbols.insert(std::make_pair(name, symRef));
    _symbolNameFromSlot.insert(std::make_pair(symRef->getCPIndex(), name));
    
@@ -432,6 +434,8 @@ void
 MethodBuilder::DefineLocal(const char *name, TR::IlType *dt)
    {
    MB_REPLAY("DefineLocal(\"%s\", %s);", name, REPLAY_TYPE(dt));
+
+   TR_ASSERT_FATAL(_symbolTypes.find(name) == _symbolTypes.end(), "Symbol '%s' already defined", name);
    _symbolTypes.insert(std::make_pair(name, dt));
    }
 
@@ -439,6 +443,8 @@ void
 MethodBuilder::DefineMemory(const char *name, TR::IlType *dt, void *location)
    {
    MB_REPLAY("DefineMemory(\"%s\", %s, " REPLAY_POINTER_FMT ");", name, REPLAY_TYPE(dt), REPLAY_POINTER(location, name));
+
+   TR_ASSERT_FATAL(_memoryLocations.find(name) == _memoryLocations.end(), "Memory '%s' already defined", name);
 
    _symbolTypes.insert(std::make_pair(name, dt));
    _memoryLocations.insert(std::make_pair(name, location));
@@ -448,6 +454,8 @@ void
 MethodBuilder::DefineParameter(const char *name, TR::IlType *dt)
    {
    MB_REPLAY("DefineParameter(\"%s\", %s);", name, REPLAY_TYPE(dt));
+
+   TR_ASSERT_FATAL(_parameterSlot.find(name) == _parameterSlot.end(), "Parameter '%s' already defined", name);
 
    _parameterSlot.insert(std::make_pair(name, _numParameters));
    _symbolNameFromSlot.insert(std::make_pair(_numParameters, name));
@@ -508,6 +516,8 @@ MethodBuilder::DefineFunction(const char* const name,
    MB_REPLAY("               " REPLAY_POINTER_FMT ",", REPLAY_POINTER(entryPoint, name));
    MB_REPLAY("               %s,", REPLAY_TYPE(returnType));
    MB_REPLAY_NONL("               %d", numParms);
+
+   TR_ASSERT_FATAL(_functions.find(name) == _functions.end(), "Function '%s' already defined", name);
 
    for (int32_t p=0;p < numParms;p++)
       {   
